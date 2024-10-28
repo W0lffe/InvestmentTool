949c88db0d985bc990a3b748bc796135e7018fc8
Henry <henry.karppinen1997@gmail.com>
2024-10-28 16:15:12 +0200

    Tested jQuery

--- IC_JS.js
+++ IC_JS.js
@@ -1,64 +1,57 @@
-
 let Data = [];
+$(document).ready(function() {
+    createList();   
+});
+
+$("#paina").click(function(){
+        $("#table").slideToggle("slow");
+})
+$("#table").hide();
+
 function createList(){
-
-const table = document.getElementById("list");
-const row = document.createElement("tr");
-
-table.innerHTML = "";
-row.innerHTML = `
-        <td>#</td>
-        <td>Deposit</td>
-        <td>Percentage</td>
-        <td>Time</td>
-        <td>After Intrest</td>
-        <td>Earnings</td>
-        <td>Volatility</td>`;
-    table.appendChild(row);
-
-
-fetch("https://www.cc.puv.fi/~e2301740/IC_Backend/IC_Backend.php",{
-    method: "GET"
-    })
-    .then(response => {
-        return response.json();
-    })
-    .then(data =>{
-        console.log("Fetched data: ");
+    $.get("https://www.cc.puv.fi/~e2301740/IC_Backend/IC_Backend.php", function(data){
         console.log(data);
         Data = data;
 
-        Data.forEach(item => {
-        let vol;
-        if(item.volatility == true){
-            vol = "Yes";
-        }else{
-            vol = "No";
-        }
+        if(data != null){
+            $("#paina").html("Data loaded. Press here");
+            $("#table").html("");
+            const row = $("<tr></tr>").html(`
+                <td>#</td>
+                <td>Deposit</td>
+                <td>Percentage</td>
+                <td>Time</td>
+                <td>After Intrest</td>
+                <td>Earnings</td>
+                <td>Volatility</td>`);
+                $("#table").append(row);
+    
+                Data.forEach(item => {
+                    let vol;
+                    if(item.volatility == true && item.type > 0){
+                        vol = "Yes";
+                    }else{
+                        vol = "No";
+                    }
+            
+                    let id = item.id;
+                    const row = $("<tr></tr>").html(`
+                    <td>${item.id}</td>
+                    <td>${item.deposit.toFixed(2)}€</td>
+                    <td>${item.percentage.toFixed(2)}%</td>
+                    <td>${item.time} ${item.period}</td>
+                    <td>${item.afterIntrest.toFixed(2)}€</td>
+                    <td>${item.earnings.toFixed(2)}€</td>
+                    <td>${vol}</td>
+                    <td> <button id="deleteButton" onclick="deleteData(${id})">Delete</button>
+                    `);
+                    $("#table").append(row);
+                });
+        };
+    });
+};
 
-        let id = item.id;
-        const row = document.createElement("tr");
-        row.innerHTML = `
-        <td>${item.id}</td>
-        <td>${item.deposit.toFixed(2)}€</td>
-        <td>${item.percentage.toFixed(2)}%</td>
-        <td>${item.time} ${item.period}</td>
-        <td>${item.afterIntrest.toFixed(2)}€</td>
-        <td>${item.earnings.toFixed(2)}€</td>
-        <td>${vol}</td>
-        <td> <button id="deleteButton" onclick="deleteData(${id})">Delete</button>
-        `
-        table.appendChild(row);
-        });
-    })
-    .catch(error => {
-        console.log("error", error);
-    })
-
-
-}
-
-function deleteData(id){
+function deleteData(id) {
     const choice = window.confirm("Are you sure you want to delete this data?");
     
     if (choice) {
@@ -69,24 +62,19 @@ function deleteData(id){
         
         if (dataToDelete !== -1) {
 
-            fetch(`https://www.cc.puv.fi/~e2301740/IC_Backend/IC_Backend.php?id=${id}`, {
-                method: "DELETE"
-            })
-            .then(response => {
-                if (response.ok) {
+            $.ajax({
+                url: `https://www.cc.puv.fi/~e2301740/IC_Backend/IC_Backend.php?id=${id}`,
+                method: "DELETE",
+                success: function() {
                     console.log("Data deleted successfully");
-                    setTimeout(() => {createList()}, 1000);
-                } else {
-                    console.error("Failed to delete data on the server");
+                    setTimeout(() => { createList(); }, 1000);
+                },
+                error: function(xhr, error) {
+                    console.error("Failed to delete data on the server:", xhr, error);
                 }
-            })
-            .catch(error => {
-                console.error("ERROR:", error);
             });
         } else {
             console.error("Data not found in the array");
         }
     }
 }
-
-createList();
